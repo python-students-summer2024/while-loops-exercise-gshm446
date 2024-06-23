8586482e18009f4e5afe938583c87651ae189d4d
monalisa <mona.lisa@louvre.org>
2024-06-23 09:26:19 +0800

    6.23

---
+++ while_loop_flag_version.py
@@ -0,0 +1,32 @@
+def get_starting_number():
+    number=input("How many bottles of beer on the wall?")
+    true=True
+    if number.isdigit():
+        number=int(number)
+        if number>=1:
+            return int(number)
+    else:
+        while true:
+            number=input("How many bottles of beer on the wall?")
+            if number.isdigit():
+                number=int(number)
+                if number>=1:
+                    true=False
+
+def sing(number):
+    true=True
+    while true:
+        if number==1:
+            true=False
+            break
+
+        elif number-1==1:
+            bottle='bottle'
+        else:
+            bottle='bottles'
+
+        print(f'{number} bottles of beer on the wall, {number} bottles of beer.\nTake one down, pass it around, {number-1} {bottle} of beer on the wall.\n')
+        number-=1
+
+
+    print('1 bottle of beer on the wall, 1 bottle of beer.\nTake it down, pass it around, no more bottles of beer on the wall!')
